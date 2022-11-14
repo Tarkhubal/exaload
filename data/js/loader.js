1eb8da2a0121e76e67104195cd89702e4c979e95
The_WildThomas <thb5309@gmail.com>
2022-11-14 11:38:38 +0100

    Réglages d'un bug du loader

--- data/js/loader.js
+++ data/js/loader.js
@@ -1,10 +1,12 @@
 var myVar;
 
 function myFunction() {
-    myVar = setTimeout(showPage, 1000);
+    myVar = setTimeout(showPage, 300);
 }
 
 function showPage() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("page").style.display = "block";
 }
+
+myFunction()
