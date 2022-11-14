08cb4ad330dfbd2b7db77e164bbb80c0645f064e
The_WildThomas <thb5309@gmail.com>
2022-11-14 17:02:01 +0100

    Test3 loader

--- data/commons/loader/index.php
+++ data/commons/loader/index.php
@@ -1,14 +1,14 @@
 <section>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
     <div class="dank-ass-loader">
-        <div class="row">
+        <div class="part">
             <div class="arrow up outer outer-18"></div>
             <div class="arrow down outer outer-17"></div>
             <div class="arrow up outer outer-16"></div>
             <div class="arrow down outer outer-15"></div>
             <div class="arrow up outer outer-14"></div>
         </div>
-        <div class="row">
+        <div class="part">
             <div class="arrow up outer outer-1"></div>
             <div class="arrow down outer outer-2"></div>
             <div class="arrow up inner inner-6"></div>
@@ -17,7 +17,7 @@
             <div class="arrow down outer outer-13"></div>
             <div class="arrow up outer outer-12"></div>
         </div>
-        <div class="row">
+        <div class="part">
             <div class="arrow down outer outer-3"></div>
             <div class="arrow up outer outer-4"></div>
             <div class="arrow down inner inner-1"></div>
@@ -26,7 +26,7 @@
             <div class="arrow up outer outer-11"></div>
             <div class="arrow down outer outer-10"></div>
         </div>
-        <div class="row">
+        <div class="part">
             <div class="arrow down outer outer-5"></div>
             <div class="arrow up outer outer-6"></div>
             <div class="arrow down outer outer-7"></div>
